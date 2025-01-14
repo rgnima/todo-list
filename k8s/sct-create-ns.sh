07e422c9c7ceb60dee5b05061f6e13e9650f570a
Ricardo NM <ricardonima@gmail.com>
2025-01-14 02:20:50 +0000

    prom y graf

--- k8s/sct-create-ns.sh
+++ k8s/sct-create-ns.sh
@@ -1,4 +1,4 @@
 if kubectl get namespace todo > /dev/null 2>&1; then
-	echo "App Namespace todo already exists."
+   echo "App Namespace todo already exists."
 else
    kubectl create namespace todo
